619b3ca37631176941645bfc2a87db552226a17b
Wang Wei <wangwei1237@gmail.com>
2020-03-15 08:43:46 +0800

    更新ayer

--- generate.sh
+++ generate.sh
@@ -8,3 +8,5 @@ $HEXO generate
 cd public && \
 rm -rf digital-video-concept && \
 cp -r /$HOME/Documents/Project/webroot/book/video digital-video-concept
+
+sed -i '' 's#<urlset.*#\<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\ <url>\ <loc>https://wangwei1237.github.io/digital-video-concept/</loc> \<lastmod>2020-03-03T02:55:11.212Z</lastmod> \ </url> #g' sitemap.xml
