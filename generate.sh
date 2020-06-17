50b382791d9902ad56cf0d3a7b10a4dc33fb6725
Wang Wei <wangwei1237@gmail.com>
2020-06-17 17:38:15 +0800

    增加图书类目

--- generate.sh
+++ generate.sh
@@ -7,8 +7,19 @@ $HEXO generate
 
 cd public && \
 rm -rf digital-video-concept && \
-cp -r /$HOME/Documents/Project/webroot/book/video digital-video-concept
+cp -r /$HOME/Documents/Project/webroot/book/video digital-video-concept && \
+rm -rf monolith-to-microservices && \
+cp -r /$HOME/Documents/Project/webroot/book/m2m monolith-to-microservices
 
-sed -i '' 's#<urlset.*#\<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\ <url>\ <loc>https://wangwei1237.github.io/digital-video-concept/</loc> \<lastmod>2020-03-03T02:55:11.212Z</lastmod> \ </url> #g' sitemap.xml
+book1="\ \ <url><loc>https://wangwei1237.github.io/digital-video-concept/</loc><lastmod>2020-06-17T02:55:11.212Z</lastmod></url>"
+book2="\ \ <url><loc>https://wangwei1237.github.io/monolith-to-microservices/</loc><lastmod>2020-06-17T02:55:11.212Z</lastmod></url>"
+
+sed -i '' "4 i\ 
+${book1} 
+" sitemap.xml
+
+sed -i '' "4 i\ 
+${book2} 
+" sitemap.xml
 
 cp sitemap.xml bd_sitemap.xml && sed -i '' 's/github/gitee/g' bd_sitemap.xml
---
+++ source/books/index.md
@@ -0,0 +1,7 @@
+---
+title: 图书资源
+date: 2020-06-17 16:50:12
+---
+
+* [**数字视频相关概念**](/digital-video-concept/)
+* [**从单体应用到微服务架构**](/monolith-to-microservices/)
