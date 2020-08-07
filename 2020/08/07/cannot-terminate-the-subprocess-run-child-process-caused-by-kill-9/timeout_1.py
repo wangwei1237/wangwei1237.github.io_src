f221f99889ccb9f26a56a71cad8cb51e0cb6bdad
Wang Wei <wangwei1237@gmail.com>
2020-08-07 17:36:20 +0800

    Site updated: 2020-08-07 17:36:18

--- 2020/08/07/cannot-terminate-the-subprocess-run-child-process-caused-by-kill-9/timeout_1.py
+++ 2020/08/07/cannot-terminate-the-subprocess-run-child-process-caused-by-kill-9/timeout_1.py
@@ -12,51 +12,42 @@ import signal
 import requests
 import subprocess
 
-process = None
-
 def sigintHandler(signum, frame):
-    print("中断发生。")
-    global process
-    process.kill();
-    print("执行最后的清理工作。")
+    raise KeyboardInterrupt
+
     exit()
 
 def run_cmd(cmd, cwd):
+    res = {}
+    str_res = ''
     signal.signal(signal.SIGTERM, sigintHandler)
-    global process
-    with subprocess.Popen(
-        cmd,
-        cwd=cwd,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        encoding="utf-8",
-    ) as process:
-        try:
-            stdout, stderr = process.communicate(input, timeout=timeout)
-        except TimeoutExpired as exc:
-            process.kill()
-            if _mswindows:
-                # Windows accumulates the output in a single blocking
-                # read() call run on child threads, with the timeout
-                # being done in a join() on those threads.  communicate()
-                # _after_ kill() is required to collect that and add it
-                # to the exception.
-                exc.stdout, exc.stderr = process.communicate()
-            else:
-                # POSIX _communicate already populated the output so
-                # far into the TimeoutExpired exception.
-                process.wait()
-            raise
-        except:  # Including KeyboardInterrupt, communicate handled that.
-            process.kill()
-            # We don't call process.wait() as .__exit__ does that for us.
-            raise
-        retcode = process.poll()
-        if retcode:
-            raise subprocess.CalledProcessError(retcode, process.args,
-                                     output=stdout, stderr=stderr)
-    return subprocess.CompletedProcess(process.args, retcode, stdout, stderr)
+
+    try:
+        res = subprocess.run(
+            cmd,
+            cwd=cwd,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            encoding="utf-8",
+            timeout=600
+        )
+        print(res)
+        print(res.returncode)
+        print(res)
+
+        if 0 == res.returncode:
+            str_res = str(res.stdout)
+        else:
+            str_res = str(res.stderr)
+
+    except KeyboardInterrupt:
+        print('receive ctrl+c or SIGINT signal')
+
+    except:
+        print('exit') 
+
+    return str_res
 
 if __name__ == '__main__':
     currentDir, _ = os.path.split(os.path.abspath(__file__))
