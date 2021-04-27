92f029c7e078f1360dc1834fec4e239c8a6bdfc2
Wang Wei <wangwei1237@gmail.com>
2021-04-27 17:49:22 +0800

    Site updated: 2021-04-27 17:49:19

--- 2020/02/28/Introduction-to-digital-video-technology/s/start_jupyter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -p 8888:8888 -v $(pwd):/home/jovyan/work jupyter/scipy-notebook
--- 2020/08/07/cannot-terminate-the-subprocess-run-child-process-caused-by-kill-9/timeout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for((i=0;;i++))
-do
-	echo "$i" > log 2>&1
-	sleep 1
-done
-
