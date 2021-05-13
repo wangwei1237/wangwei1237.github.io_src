a5b15e46219f942c6b09228c296b5f9d54685008
Wang Wei <wangwei1237@gmail.com>
2021-05-13 15:55:26 +0800

    :Update: Add release

--- build_books.sh
+++ build_books.sh
@@ -8,8 +8,6 @@ fi
 
 ROOT_PATH=$1
 
-mkdir -p ${ROOT_PATH}/books
-
 BOOKS=('chaos-engineering' 
 'digital_video_concepts' 
 'temperature-of-the-idioms' 
@@ -17,26 +15,31 @@ BOOKS=('chaos-engineering'
 'discovery-the-unpredictable-risk' 
 'Kubernetes-in-Action-Second-Edition')
 
+mkdir -p ${ROOT_PATH}/books && cd ${ROOT_PATH}/books
+
 for ((i=0; i <${#BOOKS[@]}; i++))
 do
     echo build book: ${BOOKS[i]}
     
-    cd $ROOT_PATH/${BOOKS[i]}
-    
-    gitbook install 
-    
-    if [ "${BOOKS[i]}" = "monolith-to-microservices" ]
-    then
-        /bin/cp -rf misc/gitbook-plugin-hints/* node_modules/gitbook-plugin-hints/
-        /bin/cp -rf misc/tbfed-pagefooter_index.js node_modules/gitbook-plugin-tbfed-pagefooter/index.js
-    fi
+    /bin/cp -f $ROOT_PATH/${BOOKS[i]}/release/${BOOKS[i]}.tar.gz . && tar xzvf ${BOOKS[i]}.tar.gz
 
-    if [ "${BOOKS[i]}" = "digital_video_concepts" -o ""${BOOKS[i]}"" = "temperature-of-the-idioms" ]
-    then
-        /bin/cp -rf misc/tbfed-pagefooter_index.js node_modules/gitbook-plugin-tbfed-pagefooter/index.js
-    fi
-    
-    gitbook build
-
-    /bin/cp -rf _book $ROOT_PATH/books/${BOOKS[i]}
+    #For the bugs of the gitbook, we can not build books with gitbook cmd.
+    #cd $ROOT_PATH/${BOOKS[i]}
+    #
+    #gitbook install 
+    #
+    #if [ "${BOOKS[i]}" = "monolith-to-microservices" ]
+    #then
+    #    /bin/cp -rf misc/gitbook-plugin-hints/* node_modules/gitbook-plugin-hints/
+    #    /bin/cp -rf misc/tbfed-pagefooter_index.js node_modules/gitbook-plugin-tbfed-pagefooter/index.js
+    #fi
+    #
+    #if [ "${BOOKS[i]}" = "digital_video_concepts" -o ""${BOOKS[i]}"" = "temperature-of-the-idioms" ]
+    #then
+    #    /bin/cp -rf misc/tbfed-pagefooter_index.js node_modules/gitbook-plugin-tbfed-pagefooter/index.js
+    #fi
+    #
+    #gitbook build
+    #
+    #/bin/cp -rf _book $ROOT_PATH/books/${BOOKS[i]}
 done
