572395679e872a8dd6a6c80d36b26424538e8c3f
Wang Wei <wangwei1237@gmail.com>
2021-05-13 16:42:17 +0800

    :Update: Add release

--- release.sh
+++ release.sh
@@ -24,19 +24,22 @@ done
 book1="\ \ <url><loc>https://wangwei1237.github.io/digital-video-concept/</loc><lastmod>2020-06-17T02:55:11.212Z</lastmod></url>"
 book2="\ \ <url><loc>https://wangwei1237.github.io/monolith-to-microservices/</loc><lastmod>2020-06-17T02:55:11.212Z</lastmod></url>"
 
-sed -i '' "4 i\ 
+sed -i "4 i\ 
 ${book1} 
 " sitemap.xml
 
-sed -i '' "4 i\ 
+sed -i "4 i\ 
 ${book2} 
 " sitemap.xml
 
-cp sitemap.xml bd_sitemap.xml && sed -i '' 's/github/gitee/g' bd_sitemap.xml
+cp sitemap.xml bd_sitemap.xml && sed -i 's/github/gitee/g' bd_sitemap.xml
 cp monolith-to-microservices/sitemap.xml m2m_sitemap.xml
-cp m2m_sitemap.xml m2m_e_sitemap.xml && sed -i '' 's/gitee/github/g' m2m_e_sitemap.xml
+cp m2m_sitemap.xml m2m_e_sitemap.xml && sed -i 's/gitee/github/g' m2m_e_sitemap.xml
 
 cd .. 
 cp -r docs/* public/shares
+
+git config --global user.email "wangwei1237@gmail.com"
+git config --global user.name "Wang Wei"
 
 hexo deploy
