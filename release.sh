3fca0eb1614d0321126af978a76c80d6fb368fc5
Wang Wei <wangwei1237@gmail.com>
2021-05-14 11:37:53 +0800

    :bug: Fixed: Fixed the sitemap generate bug

--- release.sh
+++ release.sh
@@ -24,13 +24,9 @@ done
 book1="\ \ <url><loc>https://wangwei1237.github.io/digital-video-concept/</loc><lastmod>2020-06-17T02:55:11.212Z</lastmod></url>"
 book2="\ \ <url><loc>https://wangwei1237.github.io/monolith-to-microservices/</loc><lastmod>2020-06-17T02:55:11.212Z</lastmod></url>"
 
-sed -i "4 i\ 
-${book1} 
-" sitemap.xml
+sed -i "4 i ${book1} " sitemap.xml
 
-sed -i "4 i\ 
-${book2} 
-" sitemap.xml
+sed -i "4 i ${book2} " sitemap.xml
 
 cp sitemap.xml bd_sitemap.xml && sed -i "s/github/gitee/g" bd_sitemap.xml
 cp monolith-to-microservices/sitemap.xml m2m_sitemap.xml
