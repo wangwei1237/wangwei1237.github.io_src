29b1c049252d5eb94dde506e8563aad1dd198994
Wang Wei <wangwei1237@gmail.com>
2021-05-21 15:35:12 +0800

    增加发布脚本

--- build_books.sh
+++ build_books.sh
@@ -16,11 +16,19 @@ BOOKS=('chaos-engineering'
 'Kubernetes-in-Action-Second-Edition')
 
 mkdir -p ${ROOT_PATH}/books && cd ${ROOT_PATH}/books
+URL=""
+DOWNLOAD_URL=""
 
 for ((i=0; i <${#BOOKS[@]}; i++))
 do
     echo build book: ${BOOKS[i]}
-    
+    if [ "${BOOKS[i]}" = "monolith-to-microservices" ]
+    then
+        URL="https://api.github.com/repos/wangwei1237/${BOOKS[i]}/releases/latest"
+        DOWNLOAD_URL=$(curl -H "Accept: application/vnd.github.v3+json" $URL | grep "${BOOKS[i]}.tar.gz" | grep 'browser_download_url' | cut -d'"' -f4)
+        wget $DOWNLOAD_URL --no-check-certificate && tar xzvf ${BOOKS[i]}.tar.gz
+    fi
+
     /bin/cp -f $ROOT_PATH/${BOOKS[i]}/release/${BOOKS[i]}.tar.gz . && tar xzvf ${BOOKS[i]}.tar.gz
 
     #For the bugs of the gitbook, we can not build books with gitbook cmd.
