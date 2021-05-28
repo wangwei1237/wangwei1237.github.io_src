82442219944e6034fd7230b857bea8c1387f9244
Wang Wei <wangwei1237@gmail.com>
2021-05-28 15:48:34 +0800

    :bug: 修复/digital_video_concepts页面的sitemap.xml链接

--- release.sh
+++ release.sh
@@ -21,7 +21,7 @@ do
     rm -rf ${BOOKS[i]} && /bin/cp -rf $ROOT_PATH/books/${BOOKS[i]} ${BOOKS[i]}
 done
 
-book1="\ \ <url><loc>https://wangwei1237.github.io/digital-video-concept/</loc><lastmod>2020-06-17T02:55:11.212Z</lastmod></url>"
+book1="\ \ <url><loc>https://wangwei1237.gitee.io/digital_video_concepts/</loc><lastmod>2020-06-17T02:55:11.212Z</lastmod></url>"
 book2="\ \ <url><loc>https://wangwei1237.github.io/monolith-to-microservices/</loc><lastmod>2020-06-17T02:55:11.212Z</lastmod></url>"
 
 sed -i "4 i ${book1} " sitemap.xml
